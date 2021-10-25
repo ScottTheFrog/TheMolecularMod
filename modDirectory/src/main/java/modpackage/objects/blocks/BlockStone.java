33b66fd241d54a42df6a5939135f17760aaf9456
SCTTTFRG <scottthefrog1@gmail.com>
2021-10-25 20:39:46 -0300

    added pickable rocks in the overworld

--- modDirectory/src/main/java/modpackage/objects/blocks/BlockStone.java
+++ modDirectory/src/main/java/modpackage/objects/blocks/BlockStone.java
@@ -3,10 +3,9 @@ package modpackage.objects.blocks;
 import modpackage.init.BlockInit;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -16,7 +15,7 @@ import net.minecraft.world.World;
 
 public class BlockStone extends BlockBase {
 
-	public static final AxisAlignedBB GROUND_STONE_AABB = new AxisAlignedBB(0.3125D,0,0.1875D,0.6875D,0.1875D,0.8125D);
+	public static final AxisAlignedBB GROUND_STONE_AABB = new AxisAlignedBB(0.375D,0,0.3125D,0.625D,0.0625D,0.625D);
 	
 	public BlockStone(String name) {
 		super(name, Material.ROCK);
@@ -39,8 +38,12 @@ public class BlockStone extends BlockBase {
 	
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){	
+		if (worldIn.isRemote) {
+			return false;
+		}
 		worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);  
-		worldIn.spawnEntity(new EntityItem(worldIn, hitX, hitY, hitZ, new ItemStack(BlockInit.GROUND_STONE)));
+		playerIn.dropItem(Item.getItemFromBlock(BlockInit.GROUND_STONE), 1);
 		return true;
+    
     }
 } 
